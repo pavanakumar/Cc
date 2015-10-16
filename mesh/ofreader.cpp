08e18fd0e641460663640fb74fcf70a1c1b6cf0b
Pavanakumar Monamauraly <p.mohanamuraly@qmul.ac.uk>
2015-10-16 12:27:00 +0100

    More data passed to Cc

--- mesh/ofreader.cpp
+++ mesh/ofreader.cpp
@@ -67,12 +67,32 @@ void get_pm_faces( int *nface, int *ninternalface,
     facelr[ i * 2 + 1 ] = global_of_mesh->mesh()->neighbour()[i] + 1; // +1 FORTRAN indexing
   } 
   /// Patch faces only have owner and no neighbour cell
-  ////// To do ....
+  for( int i=0; i<*npatch; ++i ) {
+    int start = global_of_mesh->mesh()->boundaryMesh()[i].start();
+    int size  = global_of_mesh->mesh()->boundaryMesh()[i].size();
+    for( int j=0; j<size; ++j ) {
+      facelr[ ( start + j ) * 2 ] = global_of_mesh->mesh()->boundaryMesh()[i].faceCells()[j] + 1; // +1 FORTRAN indexing
+      facelr[ ( start + j ) * 2 + 1] = 0; /// Boundary patches have 0 as their neighbour cell ID (invalid)
+    }
+  }
 }
-/**********************************************
+/******************************************************
   Read bc patches from mesh and pass it to Cc
-***********************************************/
+  Some constants:
+  ---------------
+  0 = patchstart
+  1 = patchsize
+  2 = patchtype
+  3 = patchneigh (proc ID) - only for pocessor patches
+********************************************************/
 void get_pm_patches( int *npatch, int *patchdata ) {
-  /////  To do ....
+  for( int i=0; i<*npatch; ++i ) {
+    patchdata[ i * 4 + 0 ] = global_of_mesh->mesh()->boundaryMesh()[i].start();
+    patchdata[ i * 4 + 1 ] = global_of_mesh->mesh()->boundaryMesh()[i].size();
+    //// To do ...
+//    patchdata[ i * 4 + 2 ] = global_of_mesh->mesh()->boundaryMesh()[i].size();
+//    if( global_of_mesh->mesh()->boundaryMesh()[i].type() == "processor" )
+//      patchdata[ i * 4 + 3 ] = global_of_mesh->mesh()->boundaryMesh()[i].size();
+  }
 }
 
