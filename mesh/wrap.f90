0ffda9ba4b2fec6045de030ffa67a705fbc679bc
Pavanakumar Monamauraly <p.mohanamuraly@qmul.ac.uk>
2015-10-18 21:14:13 +0100

    Fixed changes in ofreader to wrapper

--- mesh/wrap.f90
+++ mesh/wrap.f90
@@ -10,7 +10,7 @@ module Wrap
       integer(kind=c_int) :: ipar
     end subroutine init_of_mesh
 
-    subroutine get_pm_sizes( nnode, nface, ncell, npatch ) bind(C)
+    subroutine get_pm_sizes( nnode, nface, ninternalface, ncell, npatch ) bind(C)
       use iso_c_binding
       implicit none
       integer(kind=c_int) :: nnode, nface, ncell, npatch
@@ -23,10 +23,10 @@ module Wrap
       real(kind=c_double), dimension(*) :: x
     end subroutine get_pm_nodes
 
-    subroutine get_pm_faces( nface, facelr, facenode ) bind(C)
+    subroutine get_pm_faces( nface, ninternalface, facelr, facenode ) bind(C)
       use iso_c_binding
       implicit none
-      integer(kind=c_int) :: nface
+      integer(kind=c_int) :: nface, ninternalface
       integer(kind=c_int), dimension(*) :: facelr, facenode
     end subroutine get_pm_faces
 
@@ -36,6 +36,11 @@ module Wrap
       integer(kind=c_int) :: npatch
       integer(kind=c_int), dimension(*) :: patchdata
     end subroutine get_pm_faces
+
+    subroutine close_of_mesh() bind(C)
+      use iso_c_binding
+      implicit none
+    end subroutine close_of_mesh
 
   end interface  
 
