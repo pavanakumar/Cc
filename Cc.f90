1e9a56de72bde77d65a7a3441cf86af000b42e44
Pavanakumar Monamauraly <p.mohanamuraly@qmul.ac.uk>
2015-10-29 15:23:11 +0000

    Testing the tecio module

--- Cc.f90
+++ Cc.f90
@@ -1,4 +1,5 @@
 program Cc
+  use Wrap
   use Mesh
   use mpi
   implicit none
@@ -7,6 +8,10 @@ program Cc
   type(polyMesh), dimension(nlevel) :: pm
   
   call create_mg_pm( nlevel, pm, ipar )
-
+  
+  call write_cc_tecio( 1, 0, pm(nlevel)%nnode, pm(nlevel)%ncell,&
+                       pm(nlevel)%nface, pm(nlevel)%ninternalface,&
+                       pm(nlevel)%x, pm(nlevel)%facelr, pm(nlevel)%facenode )
 
 end program Cc
+
